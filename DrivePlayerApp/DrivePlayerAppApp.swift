d79748f190f5744f681ef74ca0b81334c1e9413c
Tim <spridl@mail.ru>
2025-11-09 19:36:08 +0300

    Initial Commit

---
+++ DrivePlayerApp/DrivePlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivePlayerAppApp.swift
+//  DrivePlayerApp
+//
+//  Created by Тимур on 09.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct DrivePlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
